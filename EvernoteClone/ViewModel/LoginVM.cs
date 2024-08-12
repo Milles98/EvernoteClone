c460661a53a7ae0f3085220da618b90c3badd79e
Mille Elfver <mille.elfver98@gmail.com>
2024-08-12 11:27:31 +0200

    Refactor UI: Replace MainWindow with Login and Notes
    Removed MainWindow.xaml and MainWindow.xaml.cs files.
    Added LoginVM.cs with properties and command initialization.
    Added LoginCommand.cs and RegisterCommand.cs implementing ICommand.
    Added LoginWindow.xaml and LoginWindow.xaml.cs for login UI.
    Added NotesWindow.xaml and NotesWindow.xaml.cs for notes UI.

---
+++ EvernoteClone/ViewModel/LoginVM.cs
@@ -0,0 +1,30 @@
+﻿using EvernoteClone.Model;
+using EvernoteClone.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.ViewModel
+{
+    public class LoginVM
+    {
+        private User _user;
+
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        public RegisterCommand RegisterCommand { get; set; }
+        public LoginCommand LoginCommand { get; set; }
+
+        public LoginVM()
+        {
+            RegisterCommand = new RegisterCommand(this);
+            LoginCommand = new LoginCommand(this);
+        }
+    }
+}
