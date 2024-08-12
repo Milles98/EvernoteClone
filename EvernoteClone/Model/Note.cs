8776369fb6a9db40a840e88df907f190722cb2d1
Mille Elfver <mille.elfver98@gmail.com>
2024-08-12 11:08:42 +0200

    Add project files.

---
+++ EvernoteClone/Model/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int NotebookId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
---
+++ EvernoteClone/Model/Notebook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.Model
+{
+    public class Notebook
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
