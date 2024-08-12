66883f856564ed0c6709d1e219be80fb8e8b4109
Mille Elfver <mille.elfver98@gmail.com>
2024-08-12 11:21:51 +0200

    Add DatabaseHelper class with CRUD operations
    Updated using directives to include EvernoteClone.Model and SQLite.
    Introduced DatabaseHelper class in EvernoteClone.ViewModel.Helpers.
    Added private static string dbFile for SQLite database path.
    Implemented static methods: Insert, Update, Delete, and Read.

--- EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using EvernoteClone.Model;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,64 @@ namespace EvernoteClone.ViewModel.Helpers
 {
     public class DatabaseHelper
     {
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int rows = conn.Insert(item);
+                if (rows > 0)
+                    result = true;
+            }
+
+            return result;
+        }
+
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int rows = conn.Update(item);
+                if (rows > 0)
+                    result = true;
+            }
+
+            return result;
+        }
+
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                int rows = conn.Delete(item);
+                if (rows > 0)
+                    result = true;
+            }
+
+            return result;
+        }
+
+        public static List<T> Read<T>() where T : new()
+        {
+            List<T> items;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+                items = conn.Table<T>().ToList();
+            }
+
+            return items;
+        }
     }
 }
