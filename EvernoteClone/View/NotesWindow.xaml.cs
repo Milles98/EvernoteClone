c31fac0288fa9b06273b4e798c6549a1ef2d16ce
Mille Elfver <mille.elfver98@gmail.com>
2024-08-12 11:48:37 +0200

    Refactor startup and add NotesWindow UI
    Modified App.xaml to change StartupUri to NotesWindow.xaml.
    Updated NotesWindow.xaml with new namespace, title, dimensions,
    and added a DockPanel with a Menu and an empty Grid. Added
    Window.Resources for NotesVM and set DataContext. Implemented
    MenuItem_Click event handler in NotesWindow.xaml.cs to handle
    application shutdown. Updated NewNoteCommand.cs with a TODO
    comment in the Execute method.

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace EvernoteClone.View
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
