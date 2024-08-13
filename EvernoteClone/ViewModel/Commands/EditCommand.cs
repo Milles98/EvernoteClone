3f12bcbc2ef9863a1bb64e5f3e7f94db1c8f7c0f
Mille Elfver <mille.elfver98@gmail.com>
2024-08-13 10:15:07 +0200

    Add context menu and edit functionality for notebook names
    - Updated NotesWindow.xaml to include a ContextMenu for the DisplayNotebook user control with a MenuItem for renaming, bound to an EditCommand.
    - Added a TextBox to NotesWindow.xaml for editing notebook names, with visibility bound to the IsVisible property of the view model.
    - Introduced a new Visibility property IsVisible in NotesVM.cs to control the TextBox visibility.
    - Added an EditCommand and StartEditing method in NotesVM.cs to handle renaming.
    - Created EditCommand.cs to define the EditCommand class implementing ICommand.

---
+++ EvernoteClone/ViewModel/Commands/EditCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+    public class EditCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public NotesVM ViewModel { get; set; }
+
+        public EditCommand(NotesVM vm)
+        {
+            ViewModel = vm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            ViewModel.StartEditing();
+        }
+    }
+}
